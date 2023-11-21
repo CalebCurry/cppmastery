311527597acead81d195b73f1ef87e36646d0710
Caleb <caleb@calebcurry.com>
2023-11-21 09:40:43 -0500

    basic makefile

--- advanced-c/5/tests/test.c
+++ advanced-c/5/tests/test.c
@@ -5,7 +5,23 @@
 // gcc -o linked-list-tests src/linked-list.c tests/test.c -lcriterion
 // -I/opt/homebrew/include -L/opt/homebrew/lib
 
+LinkedList* list;
+void setup(void) { list = generateLinkedList(); }
+
+void teardown(void) { freeList(list); }
+
+TestSuite(linkedlist, .init = setup, .fini = teardown);
+
 Test(linkedlist, generate) {
-    LinkedList* list = generateLinkedList();
     cr_expect(list, "linked list should not be a null pointer");
+}
+
+Test(linkedlist, pushBack) {
+    pushBack(list, 5, 10);
+    pushBack(list, 15, 20);
+
+    cr_assert(list->start->data.x == 5);
+    cr_assert(list->start->data.y == 10);
+    cr_assert(get(list, 1).x == 15 && get(list, 1).y == 20,
+              "Data should be (15, 20)");
 }
