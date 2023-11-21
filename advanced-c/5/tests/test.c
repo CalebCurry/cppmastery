5280e56645b28817ee81d5487ee9c7c38a64989e
Caleb <caleb@calebcurry.com>
2023-11-21 05:28:05 -0500

    add comment showing how to run

--- advanced-c/5/tests/test.c
+++ advanced-c/5/tests/test.c
@@ -2,6 +2,9 @@
 
 #include "../src/linked-list.h"
 
+// gcc -o linked-list-tests src/linked-list.c tests/test.c -lcriterion
+// -I/opt/homebrew/include -L/opt/homebrew/lib
+
 Test(linkedlist, generate) {
     LinkedList* list = generateLinkedList();
     cr_expect(list, "linked list should not be a null pointer");
