251e53c3108ddbe6c476f631c2b1baf0a55d5dad
Caleb Curry <caleb@calebcurry.com>
2024-02-05 13:08:46 -0500

    tic tac toe complete

--- beginner-cpp/1/arrays.cpp
+++ beginner-cpp/1/arrays.cpp
@@ -1,22 +1,5 @@
 #include <iostream>
 
-// double avg(int data[], int size) {
-//     double sum = 0;
-//     for (int i = 0; i < size; i++) {
-//         sum += data[i];
-//     }
-//     return sum / size;
-// }
-
-void printBoard(char* board, int cols, int rows) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            std::cout << board[i * cols + j] << " ";
-        }
-        std::cout << std::endl;
-    }
-}
-
 template <typename T, size_t rows, size_t cols>
 void printArray(T(data)[rows][cols]) {
     for (size_t row = 0; row < rows; row++) {
@@ -27,31 +10,74 @@ void printArray(T(data)[rows][cols]) {
     }
 }
 
+bool checkForWin(char board[][3]) {
+    return (board[0][0] == board[0][1] && board[0][1] == board[0][2] &&
+            board[0][0] != '-') ||
+           (board[1][0] == board[1][1] && board[1][1] == board[1][2] &&
+            board[1][0] != '-') ||
+           (board[2][0] == board[2][1] && board[2][1] == board[2][2] &&
+            board[2][0] != '-') ||
+           (board[0][0] == board[1][0] && board[1][0] == board[2][0] &&
+            board[0][0] != '-') ||
+           (board[0][1] == board[1][1] && board[1][1] == board[2][1] &&
+            board[0][1] != '-') ||
+           (board[0][2] == board[1][2] && board[1][2] == board[2][2] &&
+            board[0][2] != '-') ||
+           (board[0][0] == board[1][1] && board[1][1] == board[2][2] &&
+            board[0][0] != '-') ||
+           (board[0][2] == board[1][1] && board[1][1] == board[2][0] &&
+            board[0][2] != '-');
+}
+
 int main() {
     char board[][3] = {
         {'-', '-', '-'},
         {'-', '-', '-'},
         {'-', '-', '-'},
     };
-    char board2[][4] = {
-        {'-', '-', '-'},
-        {'-', '-', '-'},
-        {'-', '-', '-'},
-    };
-
-    int rows = sizeof board / sizeof board[0];
-    int cols = sizeof board[0] / sizeof board[0][0];
-
-    std::cout << rows << " " << cols << std::endl;
-
-    board[0][0] = 'x';
-    board[0][1] = 'o';
-    board[1][0] = 'x';
-    board[0][2] = 'o';
-    board[2][0] = 'x';
 
     printArray<char, 3, 3>(board);
-    printArray<char, 3, 4>(board2);
+
+    char current = 'X';
+
+    int count = 0;
+    while (true) {
+        int row = 0;
+        int col = 0;
+
+        std::cout << "Pick a row (1, 2, 3): ";
+        std::cin >> row;
+
+        std::cout << "Pick a col (1, 2, 3): ";
+        std::cin >> col;
+
+        if (row < 1 || row > 3 || col < 1 || col > 3) {
+            std::cout << "Out of bounds budddy\n";
+            continue;
+        }
+
+        if (board[row - 1][col - 1] == '-') {
+            board[row - 1][col - 1] = current;
+            count++;
+        } else {
+            std::cout << "That spot is taken! Try again\n";
+            continue;
+        }
+
+        printArray<char, 3, 3>(board);
+
+        if (checkForWin(board)) {
+            std::cout << "Player " << current << " wins!\n";
+            break;
+        }
+
+        current = current == 'O' ? 'X' : 'O';
+
+        if (count >= 9) {
+            std::cout << "There has been a tie!" << std::endl;
+            break;
+        }
+    }
 
     return 0;
 }
