7e1d568cb5e8fe04e2010fae6216e27ffaf23669
Caleb Curry <caleb@calebcurry.com>
2024-04-27 11:55:45 -0400

    templated lambdas

--- beginner-cpp/2/lambda.cpp
+++ beginner-cpp/2/lambda.cpp
@@ -1,10 +1,12 @@
+#include <array>
 #include <iostream>
+#include <vector>
 
 int main() {
-    double price = 3;
+    double price = 4;
     double max = 5;
     double purchaseCount = 0;
-    auto isCheap = [max, &purchaseCount](double price) {
+    auto isCheap = [max, &purchaseCount]<typename T>(T price) {
         if (price <= max) {
             purchaseCount++;
             return true;
@@ -14,5 +16,18 @@ int main() {
 
     std::cout << isCheap(price) << std::endl;
 
-    std::cout << purchaseCount << std::endl;
+    if (purchaseCount > 0) {
+        std::cout << "Enjoy your purchase" << std::endl;
+    }
+
+    auto example = []<typename T>(std::vector<T> data) {
+        std::cout << data[0] << std::endl;
+    };
+
+    auto data = std::vector<int>({1, 2, 3});
+
+    example(data);
+
+    auto div = []<typename T>(T a, T b) { return a / b; };
+    std::cout << div(5.7, 5.5) << std::endl;
 }
