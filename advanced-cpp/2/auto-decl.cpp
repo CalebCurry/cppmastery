c4cb7edfb21de1bd69a5ec722334a32b98238d2d
Caleb Curry <caleb@calebcurry.com>
2024-05-17 07:29:18 -0400

    auto and decl

--- advanced-cpp/2/auto-decl.cpp
+++ advanced-cpp/2/auto-decl.cpp
@@ -1,15 +1,41 @@
 #include <iostream>
 #include <vector>
 
-double calculate(double x, double y) { return x * y; }
+class Point {
+   public:
+    double x, y;
+
+    Point(double x, double y) : x(x), y(y) {}
+
+    Point operator+(const Point& other) {
+        return Point(x + other.x, y + other.y);
+    }
+};
+
+template <typename T, typename U>
+auto add(T x, U y) {
+    return x + y;
+}
+
+auto calculate(double x, double y) -> decltype(5 / 3) { return x * y; }
 
 int main() {
-    int x = 5;
-    decltype(calculate(5.5, 3.3)) y = 10;
+    // auto x = calculate(4, 3);
+    // decltype(calculate(5.5, 3.3)) y = 10;
 
-    std::vector<double> data{5, 10, 15};
+    // auto z = add(10, 5.5);
+    // std::cout << z << std::endl;
 
-    for (auto d : data) {
-        std::cout << d << std::endl;
-    }
+    Point p1(5, 10);
+    Point p2(30, 30);
+
+    Point result = add(p1, p2);
+
+    std::cout << result.x << " " << result.y << std::endl;
+
+    // std::vector<double> data{5, 10, 15};
+
+    // for (auto d : data) {
+    //     std::cout << d << std::endl;
+    // }
 }
