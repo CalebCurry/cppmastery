afec7ece97b0ce8c297e9564d0b145737295b0a1
Caleb Curry <caleb@calebcurry.com>
2024-05-19 07:33:28 -0400

    casts

--- advanced-cpp/2/casts.cpp
+++ advanced-cpp/2/casts.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
+#include <ostream>
+#include <vector>
+
+class Point {
+   protected:
+    int x, y;
+
+   public:
+    Point(int x, int y) : x(x), y(y) {}
+
+    virtual ~Point() {}
+
+    virtual void print(std::ostream& os) const {
+        os << "(" << x << ", " << y << ")";
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Point& p) {
+        p.print(os);
+        return os;
+    }
+};
+
+class PointXYZ : public Point {
+   private:
+    int z;
+
+   public:
+    PointXYZ(int x, int y, int z) : Point(x, y), z(z) {}
+
+    void print(std::ostream& os) const override {
+        os << "(" << x << ", " << y << ", " << z << ")";
+    }
+
+    void XYZOnly() { std::cout << "This requires XYZ instance" << std::endl; }
+};
+
+void processPoint(Point* p) {
+    auto result = dynamic_cast<PointXYZ*>(p);
+    if (result != nullptr) {
+        result->XYZOnly();
+    }
+}
 
 int main() {
-    int a = 10;
-    int b = 3;
-    double result = static_cast<double>(a) / b;
-    std::cout << result << std::endl;
+    PointXYZ pxyz = PointXYZ(50, 10, 30);
+    Point p = Point(50, 10);
+    std::vector<Point*> points{&pxyz, &p};
+
+    for (auto p : points) {
+        std::cout << *p << std::endl;
+        processPoint(p);
+    }
 }
