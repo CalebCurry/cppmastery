43b6b98667bd0b102729c4c4e5aba41e7f63e65c
Caleb Curry <caleb@calebcurry.com>
2024-05-27 07:32:41 -0400

    using objects with sql

--- advanced-cpp/4/customers/src/main.cpp
+++ advanced-cpp/4/customers/src/main.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
+#include <optional>
 #include <pqxx/pqxx>
+#include <vector>
+
+class Customer {
+   public:
+    std::optional<int> id;
+    std::string name;
+    std::string email;
+
+    Customer(std::optional<int> id, std::string name, std::string email)
+        : id(id), name(name), email(email) {}
+};
 
 auto getCustomers(pqxx::work& tx) {
-    return tx.query<int, std::string, std::string>(
-        "SELECT id, name, email FROM customers");
+    std::vector<Customer> customers;
+
+    for (auto [id, name, email] : tx.query<int, std::string, std::string>(
+             "SELECT id, name, email FROM customers")) {
+        customers.push_back(Customer{id, name, email});
+    }
+
+    return customers;
 }
 
-void insertCustomer(pqxx::work& tx, std::string name, std::string email) {
-    std::string query = "INSERT INTO customers (name, email) VALUES ($1, $2)";
-    // std::cout << query << std::endl;
-    tx.exec_params(query, name, email);
+void insertCustomer(pqxx::work& tx, Customer& customer) {
+    std::string query =
+        "INSERT INTO customers (name, email) VALUES ($1, $2) RETURNING id";
+    auto row = tx.exec_params1(query, customer.name, customer.email);
+    customer.id = row[0].as<int>();
 }
 
-void printCustomers(
-    pqxx::internal::result_iteration<int, std::string, std::string> customers) {
-    for (auto [id, name, email] : customers) {
-        std::cout << id << " ";
-        std::cout << name << " ";
-        std::cout << email << std::endl;
+void printCustomers(std::vector<Customer> customers) {
+    for (auto customer : customers) {
+        std::cout << *customer.id << " ";
+        std::cout << customer.name << " ";
+        std::cout << customer.email << std::endl;
     }
     std::cout << std::endl;
+}
+
+void deleteCustomer(pqxx::work& tx, int id) {
+    tx.exec_params("DELETE FROM customers WHERE id=$1", id);
 }
 
 int main() {
@@ -32,10 +54,18 @@ int main() {
 
         pqxx::work tx{c};
 
-        std::string name = "Sandra";
-        std::string email = "san@dra.tech";
+        std::string name;
+        std::string email;
 
-        insertCustomer(tx, name, email);
+        std::cout << "Provide a name and email:" << std::endl;
+        std::cin >> name;
+        std::cin >> email;
+
+        Customer customer{{}, name, email};
+
+        insertCustomer(tx, customer);
+
+        std::cout << *customer.id << std::endl;
 
         auto customers = getCustomers(tx);
 
