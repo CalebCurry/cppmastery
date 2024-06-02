8685cdd9e8eefd07416188a44cfb2ad98f620cdd
Caleb Curry <caleb@calebcurry.com>
2024-06-02 05:57:07 -0400

    if constexpr

---
+++ advanced-cpp/5/type-traits.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <type_traits>
+
+class Point {
+   public:
+    int x;
+    int y;
+
+    Point(int x, int y) : x(x), y(y) {}
+
+    Point operator+(const Point& other) {
+        return Point(this->x + other.x, this->y + other.y);
+    }
+};
+
+template <typename T, typename U>
+auto add(T x, U y) {
+    if constexpr (std::is_pointer<T>() && std::is_pointer<U>()) {
+        return *x + *y;
+    } else if constexpr (std::is_pointer<T>()) {
+        return *x + y;
+    } else if constexpr (std::is_pointer<U>()) {
+        return x + *y;
+    } else {
+        return x + y;
+    }
+}
+
+int main() {
+    int a = 10;
+    int b = 20;
+    std::cout << add(a, b) << std::endl;
+
+    int* ap = &a;
+    int* bp = &b;
+    std::cout << add(ap, 1) << std::endl;
+    std::cout << add(ap, bp) << std::endl;
+
+    Point p1(5, 10);
+    Point p2(50, 10);
+    Point p3 = add(p1, p2);
+    std::cout << p3.x << " " << p3.y << std::endl;
+}
