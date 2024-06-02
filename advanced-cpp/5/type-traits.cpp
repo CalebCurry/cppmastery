2c3ab595d3a933a90775b8ef56c91cb82d1f0cb4
Caleb Curry <caleb@calebcurry.com>
2024-06-02 07:54:19 -0400

    sfinae

--- advanced-cpp/5/type-traits.cpp
+++ advanced-cpp/5/type-traits.cpp
@@ -8,7 +8,7 @@ class Point {
 
     Point(int x, int y) : x(x), y(y) {}
 
-    Point operator+(const Point& other) {
+    Point operator+(const Point& other) const {
         return Point(this->x + other.x, this->y + other.y);
     }
 };
@@ -29,23 +29,47 @@ class Point {
 // entire function for pointers
 template <
     typename T, typename U,
-    std::enable_if<std::is_pointer<T>() && std::is_pointer<U>(), int>::type = 0>
+    std::enable_if_t<std::is_pointer<T>() && std::is_pointer<U>(), int> = 0>
 auto add(const T& x, const U& y) {
     return *x + *y;
+}
+
+// integers or other stuff
+template <typename T, typename U,
+          std::enable_if<!std::is_pointer<T>() && !std::is_pointer<U>(),
+                         int>::type = 0>
+auto add(const T& x, const U& y) {
+    return x + y;
+}
+
+template <typename T, typename U,
+          std::enable_if<std::is_pointer<T>() && !std::is_pointer<U>(),
+                         int>::type = 0>
+auto add(const T& x, const U& y) {
+    return *x + y;
+}
+
+template <typename T, typename U,
+          std::enable_if<!std::is_pointer<T>() && std::is_pointer<U>(),
+                         int>::type = 0>
+auto add(const T& x, const U& y) {
+    return x + *y;
 }
 
 int main() {
     int a = 10;
     int b = 20;
-    // std::cout << add(a, b) << std::endl;
+    std::cout << add(a, b) << std::endl;
 
     int* ap = &a;
     int* bp = &b;
     // std::cout << add(ap, 1) << std::endl;
     std::cout << add(ap, bp) << std::endl;
+    std::cout << add(ap, b) << std::endl;
+    std::cout << add(a, bp) << std::endl;
 
-    // Point p1(5, 10);
-    // Point p2(50, 10);
-    // Point p3 = add(p1, p2);
-    // std::cout << p3.x << " " << p3.y << std::endl;
+    Point p1(5, 10);
+    Point p2(50, 10);
+    Point p3 = add(p1, p2);
+    std::cout << p3.x << " " << p3.y << std::endl;
 }
