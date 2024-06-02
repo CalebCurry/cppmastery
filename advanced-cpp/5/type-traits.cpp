c910110e3ada4a2d08a42c6b628d3144395e68c2
Caleb Curry <caleb@calebcurry.com>
2024-06-02 07:36:03 -0400

    enable_if

--- advanced-cpp/5/type-traits.cpp
+++ advanced-cpp/5/type-traits.cpp
@@ -13,31 +13,39 @@ class Point {
     }
 };
 
-template <typename T, typename U>
-auto add(T x, U y) {
-    if constexpr (std::is_pointer<T>() && std::is_pointer<U>()) {
-        return *x + *y;
-    } else if constexpr (std::is_pointer<T>()) {
-        return *x + y;
-    } else if constexpr (std::is_pointer<U>()) {
-        return x + *y;
-    } else {
-        return x + y;
-    }
+// template <typename T, typename U>
+// auto add(T x, U y) {
+//     if constexpr (std::is_pointer<T>() && std::is_pointer<U>()) {
+//         return *x + *y;
+//     } else if constexpr (std::is_pointer<T>()) {
+//         return *x + y;
+//     } else if constexpr (std::is_pointer<U>()) {
+//         return x + *y;
+//     } else {
+//         return x + y;
+//     }
+// }
+
+// entire function for pointers
+template <
+    typename T, typename U,
+    std::enable_if<std::is_pointer<T>() && std::is_pointer<U>(), int>::type = 0>
+auto add(const T& x, const U& y) {
+    return *x + *y;
 }
 
 int main() {
     int a = 10;
     int b = 20;
-    std::cout << add(a, b) << std::endl;
+    // std::cout << add(a, b) << std::endl;
 
     int* ap = &a;
     int* bp = &b;
-    std::cout << add(ap, 1) << std::endl;
+    // std::cout << add(ap, 1) << std::endl;
     std::cout << add(ap, bp) << std::endl;
 
-    Point p1(5, 10);
-    Point p2(50, 10);
-    Point p3 = add(p1, p2);
-    std::cout << p3.x << " " << p3.y << std::endl;
+    // Point p1(5, 10);
+    // Point p2(50, 10);
+    // Point p3 = add(p1, p2);
+    // std::cout << p3.x << " " << p3.y << std::endl;
 }
