6f48e03a480df26d0658218ddd680f893b437f3b
Caleb Curry <caleb@calebcurry.com>
2024-06-03 06:04:15 -0400

    concepts with base class

--- advanced-cpp/5/concepts2.cpp
+++ advanced-cpp/5/concepts2.cpp
@@ -19,18 +19,40 @@ class TextFile {
     TextFile(std::string content) : content(content) {}
     std::string read() { return content; }
     void write(std::string content) { this->content = content; }
+    TextFile() {}
+};
+
+class ConfigFile : public TextFile {
+   public:
     void clear() { content = ""; }
 };
 
+class SaveData : public TextFile {};
+
+class ExampleData {
+   public:
+    void clear() {}
+};
+
 template <typename F>
-    requires requires(F x) {
-        { x.clear() } -> std::same_as<void>;
-    }
+    requires(std::is_base_of_v<TextFile, F> &&
+             requires(F x) {
+                 { x.clear() } -> std::same_as<void>;
+             })
 void processFile(F& file) {
     file.clear();
 }
 
 int main() {
     TextFile textFile("Some example data");
-    processFile(textFile);
+    // processFile(textFile);
+
+    ConfigFile config;
+    processFile(config);
+
+    SaveData save;
+    // processFile(save);
+
+    ExampleData data;
+    // processFile(data);
 }
